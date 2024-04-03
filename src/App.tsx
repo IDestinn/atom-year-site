8a04d03d9ae1794cd669ba1f89e4ddd7048728b0
Destinn <IDestinn@yandex.ru>
2024-04-03 12:12:37 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Message from './Message'
+
+function App() {
+    return <div className='flex h-screen'>
+        <div className='m-auto text-center'>
+            <Message />
+            <a href="https://example.com">Bye bye</a>
+        </div>
+    </div>;
+}
+
+export default App;
